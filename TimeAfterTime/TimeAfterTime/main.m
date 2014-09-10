6552e8b8818b4d2a4440063b13258ba5f9d4b8ea
marvinlab <m.labrador@klab.com>
2014-09-10 15:37:53 +0800

    Added file 'TimeAfterTime', my first Obj-C program

---
+++ TimeAfterTime/TimeAfterTime/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by Marvin Labrador on 9/10/14.
+//  Copyright (c) 2014 Marvin Labrador. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSDate *now = [NSDate date];
+        NSLog(@"The new date lives at %p",now);
+        
+    }
+    return 0;
+}
