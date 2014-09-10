42c0138f9a0590645f89b59d802321727fe1a15c
marvinlab <m.labrador@klab.com>
2014-09-10 11:04:25 +0800

    Add to master branch file 'functions'

---
+++ 04. Functions/04. Functions/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  04. Functions
+//
+//  Created by Marvin Labrador on 9/10/14.
+//  Copyright (c) 2014 Marvin Labrador. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+    
+    
+}
+
