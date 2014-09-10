035ecfcf68f5ec8bc305efdaa3d79a0152c04511
marvinlab <m.labrador@klab.com>
2014-09-10 13:42:57 +0800

    modified file, programmed fahrenheit to celsius

--- 06. Return/06. Return/main.c
+++ 06. Return/06. Return/main.c
@@ -7,12 +7,25 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+float lastTemperature; //Global Variable
+
+float fahrenheitFromCelsius(float cel){
+    lastTemperature = cel;
+    float fahr = cel * 1.8 + 32.0;
+    
+    printf("%f Celsius is %f Fahrenheit\n", cel, fahr);
+    return fahr;
+}
+
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    float freezeInC = 0;
+    float freezeInF = fahrenheitFromCelsius(freezeInC);
+    printf("Water freezes at %f degrees fahrenheit\n",freezeInF);
+    printf("The last temperature converted was %f\n", lastTemperature);
+    return EXIT_SUCCESS;
 }
 
