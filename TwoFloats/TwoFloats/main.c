cdd486a8b90ad6fc1b5197e3ad6a68efcf4e918a
marvinlab <m.labrador@klab.com>
2014-09-10 10:42:40 +0800

    challenge TwoFloats complete

--- TwoFloats/TwoFloats/main.c
+++ TwoFloats/TwoFloats/main.c
@@ -11,8 +11,19 @@
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    //variable declarations
+    float float_a;
+    float float_b;
+    double double_a;
+    
+    //variable assignments
+    float_a = 125.22;
+    float_b = 44.014;
+    
+    //operate
+    double_a = float_a + float_b;
+    
+    printf("The sum is %f", double_a);
     return 0;
 }
 
