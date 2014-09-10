3e1e7cceecdc4c9c2faa76c65dbd003176187840
marvinlab <m.labrador@klab.com>
2014-09-10 15:55:12 +0800

    seconds from the start of 1970

--- TimeAfterTime/TimeAfterTime/main.m
+++ TimeAfterTime/TimeAfterTime/main.m
@@ -14,7 +14,9 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         NSDate *now = [NSDate date];
-        NSLog(@"The new date lives at %@",now);
+        NSLog(@"The date is %@",now);
+        double seconds = [now timeIntervalSince1970];
+        NSLog(@"It has been %f seconds since the start of 1970.",seconds);
         
     }
     return 0;
