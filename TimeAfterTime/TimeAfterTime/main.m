dd251641dad40d35fe0798c018a83c2f8f309911
marvinlab <m.labrador@klab.com>
2014-09-11 09:59:10 +0800

    Need to commit to checkout

--- TimeAfterTime/TimeAfterTime/main.m
+++ TimeAfterTime/TimeAfterTime/main.m
@@ -15,8 +15,12 @@ int main(int argc, const char * argv[])
         
         NSDate *now = [NSDate date];
         NSLog(@"The date is %@",now);
+        
         double seconds = [now timeIntervalSince1970];
         NSLog(@"It has been %f seconds since the start of 1970.",seconds);
+        
+        NSDate *later = [now dateByAddingTimeInterval:100000];
+        NSLog(@"In 100,000 second, it will be %@", later);
         
     }
     return 0;
