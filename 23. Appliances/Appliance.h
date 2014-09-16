676cd065825e92eda5dd48e63b3deaf59c9a59df
marvinlab <m.labrador@klab.com>
2014-09-16 15:00:11 +0800

    nothing

---
+++ 23. Appliances/Appliance.h
@@ -0,0 +1,13 @@
+//
+//  Appliance.h
+//  23. Appliances
+//
+//  Created by Marvin Labrador on 9/16/14.
+//  Copyright (c) 2014 Marvin Labrador. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Appliance : NSObject
+
+@end
---
+++ 23. Appliances/Appliance.m
@@ -0,0 +1,13 @@
+//
+//  Appliance.m
+//  23. Appliances
+//
+//  Created by Marvin Labrador on 9/16/14.
+//  Copyright (c) 2014 Marvin Labrador. All rights reserved.
+//
+
+#import "Appliance.h"
+
+@implementation Appliance
+
+@end
